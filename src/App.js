eb4678a269552cf2d2225b12a98ce534f2664d4b
akicho8 <akicho8@gmail.com>
2018-01-25 18:31:16 +0900

    feat: sass 対応

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import DangerButton from './DangerButton'
+// import DangerButton from './DangerButton'
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          日本語
-        </p>
-        <DangerButton />
+        <section className="hero is-primary">
+          <div className="hero-head">
+            <nav className="navbar">
+              <div className="container">
+                {/* <img src={logo} className="App-logo" alt="logo" /> */}
+                <div className="navbar-brand">
+                  <span className="navbar-burger burger" data-target="navbarMenuHeroA">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </span>
+                </div>
+                <div id="navbarMenuHeroA" className="navbar-menu">
+                  <div className="navbar-end">
+                    <a className="navbar-item" href="https://github.com/akicho8/react-shogi-player">
+                      <span className="icon">
+                        <i className="fas fa-github"></i>
+                      </span>
+                      <span>Github</span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </nav>
+          </div>
+
+          <div className="hero-body">
+            <div className="container has-text-centered">
+              <h1 className="title">
+                shogi-player
+              </h1>
+              <h2 className="subtitle">
+              </h2>
+            </div>
+          </div>
+
+          <div className="hero-foot">
+            <nav className="tabs">
+              <div className="container">
+                <ul>
+                  <li><a href="#summary">概要</a></li>
+                  <li><a href="#examples">サンプル</a></li>
+                  <li><a href="#usage">使い方</a></li>
+                  <li><a href="#options">オプション</a></li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </section>
       </div>
     );
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,28 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import CalculatorContainer from './CalculatorContainer';
+import App from './App';
+
+// import CalculatorContainer from './CalculatorContainer';
 import registerServiceWorker from './registerServiceWorker';
-import FormDemo1 from './FormDemo1';
-import Example0010 from './Example0010';
+// import FormDemo1 from './FormDemo1';
+// import Example0010 from './Example0010';
 
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import reducer from './reducer';
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
+// import reducer from './reducer';
 
-const store = createStore(reducer);
+import 'bulma/css/bulma.css'
+
+// const store = createStore(reducer);
 
 ReactDOM.render(
-  // <App />,
-  // <Provider store={store}>
-  //   <CalculatorContainer />
-  // </Provider>,
-  // <FormDemo1 />
-  <div>
-    <Example0010 foo="1" />
-  </div>
-    ,
+  <App />,
   document.getElementById('root')
 );
 registerServiceWorker();
